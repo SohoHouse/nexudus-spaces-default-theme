bbbd9efd7d74bb67a99b690bd02512a55d3771d5
Sam Garson <samtgarson@gmail.com>
2016-07-26 15:02:41 +0100

    Working templating, inlining and macros

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,11 @@
 var gulp          = require('gulp'),
   gulpLoadPlugins = require('gulp-load-plugins'),
   $               = gulpLoadPlugins(),
-  argv            = require('yargs').argv;
+  argv            = require('yargs').argv,
+  DEFAULT_OUT     = './dest';
 
 gulp.task('build:scss', function () {
-  var dest = argv.out;
-  if (!dest) throw 'Destination required'
+  var dest = argv.out || DEFAULT_OUT;
   gulp.src('css.scss')
     .pipe($.plumber({
       errorHandler: $.notify.onError("<%= error.message %>")}))
@@ -22,15 +22,31 @@ gulp.task('build:scss', function () {
 });
 
 gulp.task('build:files', function () {
-  var dest = argv.out;
-  if (!dest) throw 'Destination required'
+  var dest = argv.out || DEFAULT_OUT;
     
   gulp.src('files/**/*')
     .pipe(gulp.dest(dest))
   console.log('Built Files to ' + dest)
 })
 
-gulp.task('default', ['build:scss', 'build:files'], function () {
+gulp.task('build:emails', function() {
+  return gulp.src('emails/partials/*.nunjucks')
+    .pipe($.plumber({
+      errorHandler: $.notify.onError("<%= error.message %>")}))
+    .pipe($.nunjucksRender({
+      path: ['emails/layouts']
+    }))
+    .pipe($.inlineSource())
+    .pipe($.inlineCss({
+      removeStyleTags: false,
+      preserveMediaQueries: true,
+      applyStyleTags: false
+    }))
+    .pipe(gulp.dest('emails'))
+});
+
+gulp.task('default', ['build:scss', 'build:files', 'build:emails'], function () {
   gulp.watch('**/*.scss', ['build:scss']);
   gulp.watch('files/**/*', ['build:files']);
+  gulp.watch(['emails/**/*.nunjucks', 'emails/**/*.css'], ['build:emails']);
 });
